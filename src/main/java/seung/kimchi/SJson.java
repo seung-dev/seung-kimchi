a4ecb5603bfd1b9891a27aa5989b4dc5f8b40928
seung <seung.dev@gmail.com>
2024-05-14 13:51:56 +0900

    fix: eidt SJson.object_mapper()

--- src/main/java/seung/kimchi/SJson.java
+++ src/main/java/seung/kimchi/SJson.java
@@ -3,6 +3,7 @@ package seung.kimchi;
 import java.util.List;
 import java.util.Map;
 
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -11,6 +12,7 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.cfg.JsonNodeFeature;
 import com.fasterxml.jackson.databind.introspect.DefaultAccessorNamingStrategy;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
@@ -20,28 +22,25 @@ import seung.kimchi.types.SLinkedHashMap;
 public class SJson {
 
 	public static ObjectMapper object_mapper() {
-//		objectMapper.getSerializerProvider().setNullKeySerializer(new JsonSerializer<Object>() {
-//			@Override
-//			public void serialize(Object value, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
-//				jsonGenerator.writeFieldName("");
-//			}
-//		});
 		return JsonMapper.builder()
-			.disable(
-					MapperFeature.AUTO_DETECT_CREATORS
-					, MapperFeature.AUTO_DETECT_FIELDS
-					, MapperFeature.AUTO_DETECT_GETTERS
-					, MapperFeature.AUTO_DETECT_IS_GETTERS
-					, MapperFeature.AUTO_DETECT_SETTERS
-					)
-			.addModule(
-					new SimpleModule("seung", Version.unknownVersion()).addAbstractTypeMapping(Map.class, SLinkedHashMap.class)
-					)
-			.accessorNaming(new DefaultAccessorNamingStrategy.Provider().withGetterPrefix("").withSetterPrefix(""))
-			.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
-			.configure(SerializationFeature.WRITE_ENUMS_USING_TO_STRING, true)
-			.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false)
-			.build();
+				.disable(
+						MapperFeature.AUTO_DETECT_CREATORS
+						, MapperFeature.AUTO_DETECT_FIELDS
+						, MapperFeature.AUTO_DETECT_GETTERS
+						, MapperFeature.AUTO_DETECT_IS_GETTERS
+						, MapperFeature.AUTO_DETECT_SETTERS
+						)
+				.addModule(
+						new SimpleModule("seung", Version.unknownVersion()).addAbstractTypeMapping(Map.class, SLinkedHashMap.class)
+						)
+				.accessorNaming(new DefaultAccessorNamingStrategy.Provider().withGetterPrefix("").withSetterPrefix(""))
+				.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+				.configure(SerializationFeature.WRITE_ENUMS_USING_TO_STRING, true)
+				.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false)
+				.configure(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS, true)
+				.configure(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN, true)
+				.configure(JsonNodeFeature.STRIP_TRAILING_BIGDECIMAL_ZEROES, true)
+				.build();
 	}
 	
 	public static String stringify(
