a5aed467e216611558aefe1a6bb77e104605fb5c
seung <seung.dev@gmail.com>
2025-02-19 18:33:50 +0900

    fix: aws s3

--- src/main/java/seung/kimchi/SS3Client.java
+++ src/main/java/seung/kimchi/SS3Client.java
@@ -6,7 +6,8 @@ import java.util.List;
 import java.util.Map;
 
 import seung.kimchi.exceptions.SException;
-import seung.kimchi.types.SS3Object;
+import seung.kimchi.types.aws.SS3Object;
+import seung.kimchi.types.aws.SS3ObjectVersion;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.awscore.exception.AwsServiceException;
@@ -18,13 +19,19 @@ import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.S3ClientBuilder;
 import software.amazon.awssdk.services.s3.model.DeleteObjectsRequest;
 import software.amazon.awssdk.services.s3.model.DeleteObjectsResponse;
+import software.amazon.awssdk.services.s3.model.DeletedObject;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.HeadObjectRequest;
+import software.amazon.awssdk.services.s3.model.HeadObjectResponse;
+import software.amazon.awssdk.services.s3.model.ListObjectVersionsRequest;
+import software.amazon.awssdk.services.s3.model.ListObjectVersionsResponse;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
 import software.amazon.awssdk.services.s3.model.ObjectIdentifier;
+import software.amazon.awssdk.services.s3.model.ObjectVersion;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+import software.amazon.awssdk.services.s3.model.S3Exception;
 import software.amazon.awssdk.services.s3.model.S3Object;
 import software.amazon.awssdk.services.s3.paginators.ListObjectsV2Iterable;
 
@@ -89,21 +96,22 @@ public class SS3Client {
 					.bucket(bucket)
 					.key(key)
 					.metadata(metadata)
+					.ifNoneMatch("*")
 					.build()
 					;
 			
-			PutObjectResponse response = s3.putObject(request, RequestBody.fromBytes(bytes));
+			s3.putObject(request, RequestBody.fromBytes(bytes));
 			
-			return SS3Object.builder()
-					.e_tag(response.eTag())
-					.build();
+			return metadata(s3, bucket, key, closeable);
 			
+		} catch (S3Exception e) {
+			if(e.statusCode() == 412) {
+				throw new SException(e, "Duplicate key is not allowed.");
+			} else {
+				throw new SException(e, "Failed to put object.");
+			}
 		} catch (SdkClientException | AwsServiceException e) {
 			throw new SException(e, "Failed to put object.");
-		} finally {
-			if(closeable) {
-				s3.close();
-			}
 		}// end of try
 		
 	}// end of upload
@@ -116,6 +124,101 @@ public class SS3Client {
 			) throws SException {
 		return put(s3, bucket, key, metadata, bytes, true);
 	}// end of upload
+	
+	public static List<SS3ObjectVersion> versions(
+			final S3Client s3
+			, final String bucket
+			, final String key
+			, final boolean closeable
+			) throws SException {
+		
+		try {
+			
+			ListObjectVersionsRequest request = ListObjectVersionsRequest.builder()
+					.bucket(bucket)
+					.prefix(key)
+					.build()
+					;
+			
+			ListObjectVersionsResponse response = s3.listObjectVersions(request);
+			
+			List<SS3ObjectVersion> items = new ArrayList<>();
+			for(ObjectVersion item : response.versions()) {
+				items.add(SS3ObjectVersion.builder()
+						.version(item.versionId())
+						.lastest(item.isLatest())
+						.content_length(item.size())
+						.last_modified(item.lastModified().toEpochMilli())
+						.md5(item.eTag())
+						.build()
+						);
+			}
+			
+			return items;
+			
+		} catch (SdkClientException | AwsServiceException e) {
+			throw new SException(e, "Failed to get metadata.");
+		} finally {
+			if(closeable) {
+				s3.close();
+			}
+		}// end of try
+		
+	}// end of versions
+	public static List<SS3ObjectVersion> versions(
+			final S3Client s3
+			, final String bucket
+			, final String key
+			) throws SException {
+		return versions(s3, bucket, key, true);
+	}// end of versions
+	
+	public static SS3Object metadata(
+			final S3Client s3
+			, final String bucket
+			, final String key
+			, final boolean closeable
+			) throws SException {
+		
+		try {
+			
+			HeadObjectRequest request = HeadObjectRequest.builder()
+					.bucket(bucket)
+					.key(key)
+					.build()
+					;
+			
+			HeadObjectResponse response = s3.headObject(request);
+			
+			String content_type = response.contentType();
+			
+			return SS3Object.builder()
+					.key(key)
+					.directory(content_type.toLowerCase().contains("directory"))
+					.content_length(response.contentLength())
+					.content_type(content_type)
+					.last_modified(response.lastModified().toEpochMilli())
+					.md5(response.eTag().replaceAll("\"", ""))
+					.version(response.versionId())
+					.misc(response.metadata())
+					.build();
+			
+		} catch (SdkClientException | AwsServiceException e) {
+			throw new SException(e, "Failed to get metadata.");
+		} finally {
+			if(closeable) {
+				s3.close();
+			}
+		}// end of try
+		
+	}// end of metadata
+	public static SS3Object metadata(
+			final S3Client s3
+			, final String bucket
+			, final String key
+			) throws SException {
+		return metadata(s3, bucket, key, true);
+	}// end of metadata
 	
 	public static SS3Object get(
 			final S3Client s3
@@ -136,11 +239,14 @@ public class SS3Client {
 			GetObjectResponse response = bytes.response();
 			
 			return SS3Object.builder()
-					.e_tag(response.eTag())
+					.key(key)
 					.bytes(bytes.asByteArray())
 					.content_length(response.contentLength())
-					.content_disposition(response.contentDisposition())
 					.content_type(response.contentType())
+					.last_modified(response.lastModified().toEpochMilli())
+					.md5(response.eTag())
+					.version(response.versionId())
+					.misc(response.metadata())
 					.build();
 			
 		} catch (SdkClientException | AwsServiceException e) {
@@ -160,10 +266,11 @@ public class SS3Client {
 		return get(s3, bucket, key, true);
 	}// end of download
 	
-	public static List<S3Object> ls(
+	public static List<SS3Object> ls(
 			final S3Client s3
 			, final String bucket
 			, final String key
+			, final boolean all
 			, final boolean closeable
 			) throws SException {
 		
@@ -174,12 +281,27 @@ public class SS3Client {
 					.prefix(key)
 					.build();
 			
-			
 			ListObjectsV2Iterable iterable = s3.listObjectsV2Paginator(request);
 			
-			List<S3Object> items = new ArrayList<>();
+			List<SS3Object> items = new ArrayList<>();
 			for(ListObjectsV2Response response : iterable) {
-				items.addAll(response.contents());
+				for(S3Object item : response.contents()) {
+					if(all) {
+						items.add(metadata(
+								s3
+								, bucket
+								, item.key()//key
+								, false//closeable
+								));
+						continue;
+					}
+					items.add(SS3Object.builder()
+							.key(item.key())
+							.last_modified(item.lastModified().toEpochMilli())
+							.md5(item.eTag().replaceAll("\"", ""))
+							.build()
+							);
+				}
 			}
 			
 			return items;
@@ -193,47 +315,61 @@ public class SS3Client {
 		}// end of try
 		
 	}// end of ls
-	public static List<S3Object> ls(
+	public static List<SS3Object> ls(
 			final S3Client s3
 			, final String bucket
 			, final String key
 			) throws SException {
-		return ls(s3, bucket, key, true);
+		return ls(s3, bucket, key, false, true);
 	}// end of ls
 	
-	public static int rm(
+	public static List<SS3Object> rm(
 			final S3Client s3
 			, final String bucket
 			, final String key
-			, final boolean rf
+			, final boolean recursive
 			, final boolean closeable
 			) throws SException {
 		
 		try {
 			
-			List<S3Object> items = ls(s3, bucket, key, false);
+			List<SS3Object> items = ls(s3, bucket, key, false, false);
 			
 			if(items.size() == 0) {
-				return 0;
+				return new ArrayList<>();
 			}
 			
-			if(rf && items.size() > 1) {
-				return 0;
+			if(!recursive && items.size() > 1) {
+				return new ArrayList<>();
 			}
 			
 //			Delete delete = Delete.builder()
 //					.objects(items.stream().map(item -> ObjectIdentifier.builder().key(item.key()).build()).toList())
 //					.build();
+//			
+//			DeleteObjectsRequest request = DeleteObjectsRequest.builder()
+//					.bucket(bucket)
+//					.delete(delete)
+//					.build();
 			
 			DeleteObjectsRequest request = DeleteObjectsRequest.builder()
 					.bucket(bucket)
-//					.delete(delete)
 					.delete(builder -> builder.objects(items.stream().map(item -> ObjectIdentifier.builder().key(item.key()).build()).toList()))
 					.build();
 			
 			DeleteObjectsResponse response = s3.deleteObjects(request);
 			
-			return response.deleted().size();
+			List<SS3Object> deleted = new ArrayList<>();
+			for(DeletedObject item : response.deleted()) {
+				System.out.println(item.toString());
+				deleted.add(SS3Object.builder()
+					.key(item.key())
+					.version(item.deleteMarkerVersionId())
+					.build()
+					);
+			}
+			
+			return deleted;
 			
 		} catch (SdkClientException | AwsServiceException e) {
 			throw new SException(e, "Failed to remove objects.");
@@ -244,13 +380,13 @@ public class SS3Client {
 		}// end of try
 		
 	}// end of rm
-	public static int rm(
+	public static List<SS3Object> rm(
 			final S3Client s3
 			, final String bucket
 			, final String key
-			, final boolean rf
+			, final boolean recursive
 			) throws SException {
-		return rm(s3, bucket, key, rf, true);
+		return rm(s3, bucket, key, recursive, true);
 	}// end of rm
 	
 }
