689c97f132308ceb093c7b234a97740295efe404
seung <seung.dev@gmail.com>
2025-11-22 14:10:51 +0900

    fix: SMath.evaludate()

--- src/main/java/seung/kimchi/SMath.java
+++ src/main/java/seung/kimchi/SMath.java
@@ -1,8 +1,13 @@
 package seung.kimchi;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import net.objecthunter.exp4j.ExpressionBuilder;
 import net.objecthunter.exp4j.function.Function;
@@ -17,25 +22,51 @@ public class SMath {
 	 *            sin(x), cos(x), tan(x), asin(x), acos(x), atan(x), sinh(x), cosh(x), tanh(x)
 	 *            log(x), log10(x), exp(x) = e^x
 	 * constants: pi, e
+	 * 
+	 * expression: ${a}+${b}+${c}
 	 */
 	public static double evaluate(
 			String expression
-			, String[] variables
+			, Set<String> variables
 			, Map<String, Double> values
+			, List<Function> functions
 			) {
 		
 		return new ExpressionBuilder(expression)
 				.variables(variables)
+				.functions(functions)
 				.build()
 				.setVariables(values)
 				.evaluate()
 				;
 	}// end of evaluate
+	
+	public static String expression(
+			String formula
+			) {
+		return formula.replaceAll("\\$\\{([^}]+)}", "$1");
+	}// end of variables
+	
+	public static Set<String> variables(
+			String formula
+			) {
+		Pattern pattern = Pattern.compile("\\$\\{([^}]+)}");
+		Matcher matcher = pattern.matcher(formula);
+		Set<String> variables = new HashSet<>();
+		while(matcher.find()) {
+			variables.add(matcher.group(1));
+		}// end of while
+		return variables;
+	}// end of variables
+	
 	public static double evaluate(
-			String expression
-			, String[] variables
+			String formula
 			, SLinkedHashMap values
 			) throws SException {
+		
+		String expression = expression(formula);
+		
+		Set<String> variables = variables(formula);
 		
 		Map<String, Double> _values = new HashMap<>();
 		
@@ -47,41 +78,8 @@ public class SMath {
 				expression
 				, variables
 				, _values
+				, Collections.emptyList()
 				);
-	}// end of evaluate
-	public static double evaluate(
-			String expression
-			, Set<String> variables
-			, SLinkedHashMap values
-			) throws SException {
-		
-		Map<String, Double> _values = new HashMap<>();
-		
-		for(String k : variables) {
-			_values.put(k, values.get_double(k));
-		}
-		
-		return evaluate(
-				expression
-				, variables.stream().toArray(String[]::new)//variables
-				, _values
-				);
-	}// end of evaluate
-	
-	public static double evaluate(
-			String expression
-			, String[] variables
-			, Map<String, Double> values
-			, Function[] functions
-			) throws SException {
-		
-		return new ExpressionBuilder(expression)
-				.variables(variables)
-				.functions(functions)
-				.build()
-				.setVariables(values)
-				.evaluate()
-				;
 	}// end of evaluate
 	
 	/**
